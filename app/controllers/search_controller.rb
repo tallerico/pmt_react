013c47f555ecd969b90868d3c23e9647372261bb
Mike Tallerico <mike@talleri.co>
2018-09-26 05:45:31 -0400

    sets up search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,21 @@
 class SearchController < ApplicationController
+
+  def search
+    products = Product.where(name: params[:name]).joins(:properties, :product_properties).map do |product|
+      {
+        name: product.name,
+        upc: product.upc,
+        available_on: product.available_on,
+        properties: product.product_properties.map do |product_property|
+          {
+            name: product_property.property.name,
+            value: product_property.value
+          }
+        end
+      }
+    end.to_json  
+    
+    render json: products
+  end
+
 end
