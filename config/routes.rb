333642daf74b2f7c3739a8730707e8b1ad7b940b
Mike Tallerico <mike@talleri.co>
2018-09-26 05:47:48 -0400

    sets up route for search

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   match '/products', to: 'pages#index', via: :all
   match '/send_data' => 'send_data#save', via: :post
   match '/get_data' => 'get_data#pull', via: :get
+  match '/search' => 'search#search', via: :get
 end
