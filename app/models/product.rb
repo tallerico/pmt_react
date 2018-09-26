7b585630f9e21723e55c8913d3c7a90cbc60a98b
Mike Tallerico <mike@talleri.co>
2018-09-26 08:14:00 -0400

    starts implementation of validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3 }
   has_many :product_properties
   has_many :properties, through: :product_properties
 end
