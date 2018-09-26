749cd9858b0e25b6bbacbf078c782ff04695a97c
Mike Tallerico <mike@talleri.co>
2018-09-26 12:56:30 -0400

    sets up validation for all models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, length: { maximum: 1024 }, uniqueness: true
+  validates :upc, presence: true, numericality: { only_integer: true }, :check_for_length, uniqueness: true
   has_many :product_properties
   has_many :properties, through: :product_properties
+
+  def check_for_length
+    errors.add(:upc, :wrong_length) unless [10,12,13].include?(upc.length)
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,5 @@
 class Property < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
   has_many :product_properties
   has_many :products, through: :product_properties
   accepts_nested_attributes_for :product_properties
